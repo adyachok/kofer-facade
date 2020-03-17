078cbd4fcbd74561af7f274f6374a48657e1549d
Andras Gyacsok <andras.gyacsok@boehringer-ingelheim.com>
2020-03-17 20:12:49 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ModelListComponent } from './model-list/model-list.component';
+import { ModelDetailComponent } from './model-detail/model-detail.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModelListComponent,
+    ModelDetailComponent,
+    TaskListComponent,
+    TaskDetailComponent
   ],
   imports: [
     BrowserModule
